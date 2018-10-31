6352039c1ded83fbeac0e032a79903f154f32e1d
Martin Helmich <m.helmich@mittwald.de>
2018-10-31 09:54:20 +0100

    Update last observed time

--- watcher/template_watch_poll.go
+++ watcher/template_watch_poll.go
@@ -36,6 +36,8 @@ func (t *pollingTemplateWatcher) watch(updates chan []byte, errors chan error) {
 		if stat.ModTime() != t.lastObservedTimestamp {
 			glog.V(6).Infof("observed new modification time on %s", t.filename)
 
+			t.lastObservedTimestamp = stat.ModTime()
+
 			content, err := ioutil.ReadFile(t.filename)
 			if err != nil {
 				glog.Warningf("error while reading file %s: %s", t.filename, err.Error())
