afdb15fa47a41eaaca3dc15cf4789f70c5401044
Vadym Myrgorod <dealancer@gmail.com>
2020-03-06 12:30:16 -0500

    Issue #7: add compatibility by reading backend-port flag

--- flags.go
+++ flags.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"time"
+
+	"github.com/golang/glog"
 )
 
 type KubeHTTPProxyFlags struct {
@@ -23,6 +25,7 @@ type KubeHTTPProxyFlags struct {
 		Watch     bool
 		Namespace string
 		Service   string
+		Port      string
 		PortName  string
 	}
 	Admin struct {
@@ -54,6 +57,7 @@ func (f *KubeHTTPProxyFlags) Parse() error {
 	flag.BoolVar(&f.Backend.Watch, "backend-watch", false, "watch for Kubernetes backend updates")
 	flag.StringVar(&f.Backend.Namespace, "backend-namespace", "", "name of Kubernetes backend namespace")
 	flag.StringVar(&f.Backend.Service, "backend-service", "", "name of Kubernetes backend service")
+	flag.StringVar(&f.Backend.Port, "backend-port", "", "deprecated: name of backend port")
 	flag.StringVar(&f.Backend.PortName, "backend-portname", "http", "name of backend port")
 
 	flag.StringVar(&f.Admin.Address, "admin-addr", "127.0.0.1", "TCP address for the admin port")
@@ -65,6 +69,11 @@ func (f *KubeHTTPProxyFlags) Parse() error {
 	flag.BoolVar(&f.Varnish.VCLTemplatePoll, "varnish-vcl-template-poll", false, "poll for file changes instead of using inotify (useful on some network filesystems)")
 
 	flag.Parse()
+
+	if len(f.Backend.Port) > 0 {
+		f.Backend.PortName = f.Backend.Port
+		glog.Warningf("-backend-port flag has been deprecated in favor of -backend-portname and will be removed in future versions")
+	}
 
 	f.Kubernetes.RetryBackoff, err = time.ParseDuration(f.Kubernetes.RetryBackoffString)
 	if err != nil {
