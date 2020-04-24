bfdb40191e39b927b1510a619dab9c3b0caef0d6
Travis Terry <travisterry@iheartmedia.com>
2020-04-24 12:27:51 -0400

    build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+VERSION=$1
+
+if [ "${VERSION}" == "" ]; then
+    echo "usage: $0 <version>"
+fi
+
+echo "Building cortex-proxyd ${VERSION}"
+
+# clean check
+STATUS=$(git status --porcelain)
+if [ "${STATUS}" != "" ]; then
+    echo ${STATUS}
+    echo "Repository is not clean"
+    exit 1
+fi
+
+# git tagging
+git tag ${VERSION}
+git push --tags
+
+# docker build
+docker build -t re-docker-registry.ihrprod.net/kube-httpcache:${VERSION} .
+docker push re-docker-registry.ihrprod.net/kube-httpcache:${VERSION}
