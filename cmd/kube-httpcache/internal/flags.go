6804eb157bb28e955a5ff6365c3b7c96aecbe81b
Martin Helmich <m.helmich@mittwald.de>
2020-08-25 08:32:10 +0200

    Keep flags for BC

--- cmd/kube-httpcache/internal/flags.go
+++ cmd/kube-httpcache/internal/flags.go
@@ -48,6 +48,10 @@ type KubeHTTPProxyFlags struct {
 		VCLTemplate          string
 		VCLTemplatePoll      bool
 	}
+	Readiness struct {
+		Enable  bool
+		Address string
+	}
 }
 
 func (f *KubeHTTPProxyFlags) Parse() error {
@@ -85,6 +89,11 @@ func (f *KubeHTTPProxyFlags) Parse() error {
 	flag.StringVar(&f.Varnish.VCLTemplate, "varnish-vcl-template", "/etc/varnish/default.vcl.tmpl", "VCL template file")
 	flag.StringVar(&f.Varnish.AdditionalParameters, "varnish-additional-parameters", "", "Additional Varnish start parameters (-p, seperated by comma), like 'ban_dups=on,cli_timeout=30'")
 	flag.BoolVar(&f.Varnish.VCLTemplatePoll, "varnish-vcl-template-poll", false, "poll for file changes instead of using inotify (useful on some network filesystems)")
+
+	// present for BC only; no effect until #36 [1] has resolved
+	//   [1]: https://github.com/mittwald/kube-httpcache/issues/36
+	flag.BoolVar(&f.Readiness.Enable, "readiness-enable", true, "enable readiness probe")	
+	flag.StringVar(&f.Readiness.Address, "readiness-addr", "0.0.0.0:9102", "address for the readiness probe to listen on")
 
 	flag.Parse()
 
