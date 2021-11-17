aa110ab08a23fe55ea9d000501c0cc649516e7cf
mqmr <60264368+mqmr@users.noreply.github.com>
2021-11-17 13:29:03 +0000

    Instrument Signaller controller with prometheus-client (#111)
    This commit instruments Signaller controller with Prometheus client [1]
    for increasing observability of kube-httpcache.
    
    [1] https://github.com/prometheus/client_golang
    
    The commit adds metrics for exposing:
    * a number of requests and errors served by Signaller
    * Signaller's response time
    * a number of errors for upstream requests
    * the length and latency on Signaller's queue
    
    Co-authored-by: mqmr <me@mqmr.com>

--- go.mod
+++ go.mod
@@ -8,25 +8,26 @@ require (
 	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
-	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
+	github.com/google/gofuzz v1.0.0 // indirect
 	github.com/googleapis/gnostic v0.2.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
-	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/martin-helmich/go-varnish-client v0.2.1
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/prometheus/client_golang v1.11.0
 	github.com/spf13/pflag v1.0.3 // indirect
-	golang.org/x/crypto v0.0.0-20181012144002-a92615f3c490 // indirect
-	golang.org/x/net v0.0.0-20181011144130-49bb7cea24b1 // indirect
-	golang.org/x/oauth2 v0.0.0-20181003184128-c57b0facaced // indirect
-	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
-	golang.org/x/sys v0.0.0-20181011152604-fa43e7bc11ba // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
 	golang.org/x/time v0.0.0-20180412165947-fbb02b2291d2 // indirect
-	google.golang.org/appengine v1.2.0 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 	k8s.io/api v0.0.0-20181013054003-e94254e9898f
 	k8s.io/apimachinery v0.0.0-20181013010248-dcb88206cd7f
 	k8s.io/client-go v9.0.0+incompatible
--- pkg/signaller/run.go
+++ pkg/signaller/run.go
@@ -11,30 +11,75 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/mittwald/kube-httpcache/pkg/watcher"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	signallerRequestsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "kube_httpcache_signaller_requests_total",
+		Help: "The total number of incoming requests to Signaller",
+	})
+
+	signallerErrorsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "kube_httpcache_signaller_errors_total",
+		Help: "The total number of errors for incomming requests to Signaller",
+	})
+
+	signallerResponseTime = promauto.NewSummary(prometheus.SummaryOpts{
+		Name:       "kube_httpcache_signaller_durations_seconds",
+		Help:       "The Signaller response time",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	})
+
+	signallerUpstreamErrorsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "kube_httpcache_signaller_upstream_errors_total",
+		Help: "The total number of errors for outgoing requests to upstreams",
+	})
+
+	signallerQueueLength = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "kube_httpcache_signaller_queue_length",
+		Help: "The length of signaller queue",
+	})
+
+	signallerQueueLatency = promauto.NewSummary(prometheus.SummaryOpts{
+		Name:       "kube_httpcache_signaller_queue_latency",
+		Help:       "The Signaller queue latency",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	})
 )
 
 func (b *Signaller) Run() error {
 	server := &http.Server{
-		Addr:    b.Address + ":" + strconv.Itoa(b.Port),
-		Handler: b,
+		Addr: b.Address + ":" + strconv.Itoa(b.Port),
 	}
 
 	for i := 0; i < b.WorkersCount; i++ {
 		go b.ProcessSignalQueue()
 	}
 
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/", b.Serve)
+
 	return server.ListenAndServe()
 }
 
-func (b *Signaller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (b *Signaller) Serve(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		b.errors <- err
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		signallerErrorsTotal.Inc()
 		return
 	}
 
 	glog.V(5).Infof("received a signal request: %+v", r)
+	signallerRequestsTotal.Inc()
 
 	b.mutex.RLock()
 	endpoints := make([]watcher.Endpoint, len(b.endpoints.Endpoints))
@@ -50,10 +95,16 @@ func (b *Signaller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		request.Header = r.Header.Clone()
 		request.Host = r.Host
 		request.Header.Set("X-Forwarded-For", r.RemoteAddr)
+		signallerQueueLength.Set(float64(len(b.signalQueue)))
+		tt := time.Now()
+
 		b.signalQueue <- Signal{request, 0}
+
+		signallerQueueLatency.Observe(time.Since(tt).Seconds())
 	}
 
 	fmt.Fprintf(w, "Signal request is being broadcasted.")
+	signallerResponseTime.Observe(time.Since(t).Seconds())
 }
 
 func (b *Signaller) ProcessSignalQueue() {
@@ -82,11 +133,11 @@ func (b *Signaller) ProcessSignalQueue() {
 		response, err := client.Do(signal.Request)
 		if err != nil {
 			glog.Errorf("signal broadcast error: %v", err.Error())
-			glog.Infof("retrying in %v", b.RetryBackoff)
+			signallerUpstreamErrorsTotal.Inc()
 			b.Retry(signal)
 		} else if response.StatusCode >= 400 && response.StatusCode <= 599 {
 			glog.Warningf("signal broadcast error: unusual status code from %s: %v", response.Request.URL.Host, response.Status)
-			glog.Infof("retrying in %v", b.RetryBackoff)
+			signallerUpstreamErrorsTotal.Inc()
 			b.Retry(signal)
 		} else {
 			glog.V(5).Infof("received a signal response from %s: %+v", response.Request.URL.Host, response)
@@ -108,6 +159,7 @@ func (b *Signaller) Retry(signal Signal) {
 	signal.Attempt++
 	if signal.Attempt < b.MaxRetries {
 		go func() {
+			glog.Infof("retrying in %v", b.RetryBackoff)
 			time.Sleep(b.RetryBackoff)
 			b.signalQueue <- signal
 		}()
